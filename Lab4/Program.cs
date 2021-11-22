fe4f1289b8916a1f014ebe1b87adaadeece3f755
79138 <79138@LAPTOP-L5TVFBD5>
2021-11-22 14:25:45 +0700

    Добавьте файлы проекта.

---
+++ Lab4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            uint N = Convert.ToUInt32(Console.ReadLine());
+            uint N2;
+            for (uint i = 0; i <= N; i++)
+
+            {
+
+                Console.WriteLine("Квадрат числа {0} равен {1}", i , N2 = i * i);
+            }
+            
+           
+            Console.ReadKey();
+        }
+    }
+}
